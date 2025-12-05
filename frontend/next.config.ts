ba05cc09e127dbbc0916bebb9f33809ab3fe5ba4
Asish <asishvenkat.a2004@gmail.com>
2025-12-05 22:26:04 +0530

    fix: OAuth popup issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -25,11 +25,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "unsafe-none",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none",
+            value: "same-origin-allow-popups",
           },
         ],
       },
